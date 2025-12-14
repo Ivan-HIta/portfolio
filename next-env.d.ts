708cf30a25f4110b0796cf1d57bcc6efe5305fc2
Iván Moisés Hita <69392968+Ivan-HIta@users.noreply.github.com>
2025-12-14 03:49:21 -0800

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: The above reference directives must remain at the top of this file.
+// They enable Next.js type definitions and global image types without
+// polluting the rest of your project. Do not remove or modify them.
